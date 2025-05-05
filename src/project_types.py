dde5d98ca5bd23716836bd03e941dfbd04b19a36
HoweZae <jdtomanan@up.edu.ph>
2025-05-05 20:46:03 +0800

    feat: Import BaseModel; Initialize data type for models

--- src/project_types.py
+++ src/project_types.py
@@ -1,10 +1,25 @@
 from dataclasses import dataclass
 from typing import Protocol
 from enum import Enum, StrEnum, auto
+from pydantic import BaseModel
 
 
-class ObservationPoint():
-    ...
+class ObservationPoint(BaseModel):
+    """
+    Construct for image being passed
+    """
+    device_id: int
+    name: str
+    latitude: float
+    longitude: float
 
-class Location():
-    ...
+class Message(BaseModel):
+    """
+    Data type of message passed by microcontroller
+    """
+    device_id: int
+    sequence: int
+    gas_value: int
+    pm1_0: int
+    pm2_5: int
+    pm10_0: int
