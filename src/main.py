eefabdb01e024743668c883e3aadef4e401619d6
HoweZae <jdtomanan@up.edu.ph>
2025-05-05 20:45:33 +0800

    feat: Import directory of templates for SSR; Create get_point function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI, HTTPException
-from project_types import ObservationPoint, Location
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from project_types import ObservationPoint
 
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="../templates")
+
 observation_points: list[ObservationPoint] = []
 
+"""
+Result when accessing root of site
+"""
 @app.get("/") 
 def root():
-    return {"air": "bad"}
+    return templates.TemplateResponse("root.html")
 
 
 """
@@ -33,11 +39,13 @@ def delete_point(point: ObservationPoint):
 Given location ID, return corresponding observation point 
 Should include air quality related details
 """
-@app.post("/points/{loc_id}")
-def get_point(id: int):
-    if ...:
-        ...
-        
-    else:
-        raise HTTPException(status_code=404, detail="Observation point does not exist")
+@app.post("/points/{device_id}")
+def get_point(device_id: int):
+    ...
 
+    returnable = templates.TemplateResponse(
+        "index.html",
+        {"device_id": device_id, }
+        )
+
+    return returnable
