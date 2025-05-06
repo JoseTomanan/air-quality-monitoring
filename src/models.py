4a7fc4404eafb3158692fba0a8ce668b1d493297
HoweZae <jdtomanan@up.edu.ph>
2025-05-06 12:14:35 +0800

    refactor: Separate models from types

---
+++ src/models.py
@@ -0,0 +1,22 @@
+from sqlmodel import SQLModel
+
+
+class ObservationPoint(SQLModel):
+    """
+    Construct for image being passed
+    """
+    device_id: int
+    name: str
+    latitude: float
+    longitude: float
+
+class Message(SQLModel):
+    """
+    Data type of message passed by microcontroller
+    """
+    device_id: int
+    sequence: int
+    gas_value: int
+    pm1_0: int
+    pm2_5: int
+    pm10_0: int
--- src/project_types.py
+++ src/project_types.py
@@ -1,25 +1,3 @@
 from dataclasses import dataclass
 from typing import Protocol
 from enum import Enum, StrEnum, auto
-from sqlmodel import SQLModel
-
-
-class ObservationPoint(SQLModel):
-    """
-    Construct for image being passed
-    """
-    device_id: int
-    name: str
-    latitude: float
-    longitude: float
-
-class Message(SQLModel):
-    """
-    Data type of message passed by microcontroller
-    """
-    device_id: int
-    sequence: int
-    gas_value: int
-    pm1_0: int
-    pm2_5: int
-    pm10_0: int
