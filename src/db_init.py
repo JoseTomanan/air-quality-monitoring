83fb5ae0348f02acea4030edef9b2662f6ee3d0d
HoweZae <jdtomanan@up.edu.ph>
2025-05-06 12:44:09 +0800

    init: DB init

---
+++ src/db_init.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, create_engine
+from models import *
+
+
+def init_db_tables():
+    """
+    Initialize database, tables; Use create_engine and SQLite
+    """
+    ...
