66b563cadd00b7fa3bc3cd70e59a5a12a66c0c97
HoweZae <jdtomanan@up.edu.ph>
2025-05-08 10:04:42 +0800

    init: Schemas for response/request format

---
+++ app/schemas.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from sqlmodel import SQLModel
+
+from enums import *
+
+
+class ObservationPointAdd(SQLModel):
+    """
+    Request format for add_point
+    """
+    location_name: str
+    latitude: float
+    longitude: float
+
+class ObservationPointDelete(SQLModel):
+    """
+    Request format for delete_point()
+    """
+    device_id: int
+
+class AirDataSend(SQLModel):
+    """
+    Request format for send_air_data()
+    """
+    device_id: int
+    sequence: int
+    timestamp: datetime
+    gas_value: int
+    pm1_0: int
+    pm2_5: int
+    pm10_0: int
+
+class AirDataGet(SQLModel):
+    """
+    Response format for get_air_data()
+    """
+    device_id: int
+    location_name: str
+    latitude: float
+    longitude: float
+    status: AirStatus
