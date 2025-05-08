e39b15dcff0335496d3189cdce8fd4af74419ecb
HoweZae <jdtomanan@up.edu.ph>
2025-05-08 10:05:11 +0800

    feat: Placeholder for compute_air_status()

--- app/services/sensor.py
+++ app/services/sensor.py
@@ -1,3 +1,10 @@
 from sqlmodel import Session, select
-from models import Message
+from models import AirData
 
+
+def compute_air_status():
+    """
+    Take x most recent entries and compute for air quality;
+    Done every time a user makes a request
+    """
+    ...
