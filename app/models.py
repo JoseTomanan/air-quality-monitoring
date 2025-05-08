a0e9b3bf1dd77682ef167c1e880d890b57ad213d
HoweZae <jdtomanan@up.edu.ph>
2025-05-08 10:06:09 +0800

    feat: Rename to AirData; Make sequence number primary key; Import enums; Rename to location_name

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from enums import *
 
 
 class ObservationPoint(SQLModel, table=True):
@@ -8,16 +9,16 @@ class ObservationPoint(SQLModel, table=True):
     Construct for image being passed
     """
     device_id: Optional[int] = Field(primary_key=True)
-    name: str = Field(index=True, nullable=True)
+    location_name: str = Field(index=True, nullable=True)
     latitude: float = Field()
     longitude: float = Field()
 
-class Message(SQLModel, table=True):
+class AirData(SQLModel, table=True):
     """
     Data type of message passed by microcontroller
     """
     device_id: Optional[int] = Field(primary_key=True)
-    sequence: int = Field()
+    sequence: int = Field(primary_key=True)
     timestamp: datetime = Field()
     gas_value: int = Field()
     pm1_0: int = Field()
