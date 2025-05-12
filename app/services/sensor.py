28ace70b9b087344678a1c2d70e33d06c128a2e5
HoweZae <jdtomanan@up.edu.ph>
2025-05-12 22:07:24 +0800

    feat: Add params

--- app/services/sensor.py
+++ app/services/sensor.py
@@ -1,10 +1,13 @@
 from sqlmodel import Session, select
 from models import AirData
+from enums import AirStatus
 
 
-def compute_air_status():
+def compute_air_status(gas_conc: float, particle_conc: float) -> AirStatus:
     """
     Take x most recent entries and compute for air quality;
     Done every time a user makes a request
     """
     ...
+
+    # TODO: DEFINE
