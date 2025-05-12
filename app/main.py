ee61cd45c313ffdf3d40a7684a7fa5f14e3f9c83
Jose Tomanan <jdtomanan@up.edu.ph>
2025-05-12 11:48:23 +0800

    feat: Prevent ambiguity with schemas module (vs library)

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from models import *
-from schemas import *
+from .schemas import *
 from database import *
 from enums import *
 
