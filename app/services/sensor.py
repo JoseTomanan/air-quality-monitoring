bccc80ed689eeb7d48a874ac18a7894bc0774ade
HoweZae <jdtomanan@up.edu.ph>
2025-05-15 16:10:37 +0800

    fix: Segregate compute_conc_values into different functions

--- app/services/sensor.py
+++ app/services/sensor.py
@@ -21,14 +21,24 @@ def compute_air_status(gas_conc: float, particle_conc: float) -> AirStatus:
     return AirStatus.good
 
 
-def compute_conc_values() -> tuple[float, float]:
+def compute_gas_conc() -> float:
     """
-    Fetch last 10 values from AirData;
-    Compute for mean of gas concentration, particle concentration
+    Fetch last 10 values of gas_value from AirData;
+    Compute for mean of gas concentration and return
     """
     # TODO : define fetching logic; import database-related modules/libraries
     # (or alternatively, dyt it would be more elegant to move this to databases.py ???)
-    
     ...
+    return 0.6
 
-    return (0.6, 0.9)
+
+def compute_particle_conc() -> tuple[float, float, float]:
+    """
+    Fetch last 10 values of PM1.0, PM2.5, and PM10.0 from AirData;
+    Compute for mean of each one then return as a tuple.
+    The tuple that will be returned contain, in order: (PM1.0, PM2.5, PM10.0).
+    """
+    # TODO : define fetching logic; import database-related modules/libraries
+    # (or alternatively, dyt it would be more elegant to move this to databases.py ???)
+    ...
+    return (0.1, 0.2, 0.3)
