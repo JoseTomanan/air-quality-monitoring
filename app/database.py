6daa71888bd0203a05e834376e1be5f2450ab746
asopungco <andrew.sopungco@gmail.com>
2025-05-18 16:32:23 +0800

    refactor: moved compute_gas_conc() and compute_particle_conc() to database.py

--- app/database.py
+++ app/database.py
@@ -77,3 +77,25 @@ def update_air_data(data: AirData):
         session.add(data)
         session.commit()
     print("New air data received.")
+
+def compute_gas_conc() -> float:
+    """
+    Fetch last 10 values of gas_value from AirData;
+    Compute for mean of gas concentration and return
+    """
+    # TODO : define fetching logic; import database-related modules/libraries
+    # (or alternatively, dyt it would be more elegant to move this to databases.py ???)
+    ...
+    return 0.6
+
+
+def compute_particle_conc() -> tuple[float, float, float]:
+    """
+    Fetch last 10 values of PM1.0, PM2.5, and PM10.0 from AirData;
+    Compute for mean of each one then return as a tuple.
+    The tuple that will be returned contain, in order: (PM1.0, PM2.5, PM10.0).
+    """
+    # TODO : define fetching logic; import database-related modules/libraries
+    # (or alternatively, dyt it would be more elegant to move this to databases.py ???)
+    ...
+    return (0.1, 0.2, 0.3)
